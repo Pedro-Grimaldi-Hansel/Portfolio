f029f561b2ca8f791a7a1316b1d196deacc0e608
Pedro Grimaldi <pedroghansel@gmail.com>
2023-03-07 15:26:57 -0300

    estilização na sobre mim

--- src/pages/Sobre.tsx
+++ src/pages/Sobre.tsx
@@ -13,7 +13,7 @@ const Sobre = () => {
       <>
         <NavBarMobile/>
         <main className={styles.Sobre}>
-
+        
           <header>
             <Image alt='Foto da RR' src={foto2}/>
           </header>
@@ -25,17 +25,21 @@ const Sobre = () => {
             <hr/>
 
             <p>Sou uma pessoa muito comunicativa, com vontade de aprender e dedicação para me manter aprendendo cada dia mais.</p>
+            <hr/>
 
             <p>Como desenvolvedor tenho o trabalho de apresentar codigos de facil manutenibilidade, que respeitem determinada
                 arquitetura e atendam todas as necessidades dos clientes de forma intuitiva e com um otimo desenvolvimento de UX e Ui.</p>
+            <hr/>
 
             <p>Já como Gerente de Projetos acompanho o projeto desde sua precificação, desenvolvimento de sprints e alocação de
                 desenvolvedores, apos o inicio do projeto sou encarregado de delegar tarefas aos desenvolvedores, auxilia-los e
                 orienta-los em possiveis dificuldades e unir em todas as atuais ramificações do codigo em um arquivo principal,
                 sempre garantindo a qualidade do produto.</p>
+            <hr/>
 
             <p>Tive a oportunidade de participar de alguns eventos empresariais que me permitiram ampliar minha rede de contatos e
                 conhecer pessoas influentes na minha área, e melhorar minhas Soft-Skills.</p>
+            
           </section>
         
         </main>
