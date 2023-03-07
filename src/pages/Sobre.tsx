eaa966dd236dd5fb0542975864cb768180d9a79f
Pedro Grimaldi <pedroghansel@gmail.com>
2023-03-07 14:11:23 -0300

    alterações incompletas na pagina sobre

--- src/pages/Sobre.tsx
+++ src/pages/Sobre.tsx
@@ -1,56 +1,44 @@
 import type { NextPage } from 'next'
-import Navbar from '@/src/components/NavBarMobile'
+
 import styles from '@/src/styles/scss/sobre.module.scss'
 import TutuloPaginaMobile from '@/src/components/TutuloPaginaMobile'
 import Image from "next/image";
 
 import foto1 from '@/src/public/images/EMEJFimedited.jpeg'
 import foto2 from '@/src/public/images/RRPaineledited.jpeg'
+import NavBarMobile from '@/src/components/NavBarMobile';
 
 const Sobre = () => {
     return (  
       <>
+        <NavBarMobile/>
+        <main className={styles.Sobre}>
 
-        <TutuloPaginaMobile pagina="Sobre Mim"/>
-        
-        <section className={styles.Sobre}>
-
-          <section className={styles.Top}>
-            <p>Olá meu nome é Pedro, tenho 19 anos sou estudante e trabalho como desenvolvedor e gerente de projetos.</p>
-          </section>
-
-          <section className={styles.Mid}>
-
-            <div className={styles.Mid_Texto}>
-              <p>Sou uma pessoa muito comunicativa, com vontade de aprender e muita dedicação para me manter aprendendo cada dia mais.</p>
-            </div>
-
-            <div className={styles.Mid_foto}>
-
-              <Image alt='Foto do EMEJ' src={foto1}/>
-        
-            </div>
-
-          </section>
-          
-          <section className={styles.Bottom}>
-            <p>Acredito que uns dos momentos profissionais mais importantes para mim foram ter participado de alguns
-            eventos empresariais que me permitiram ampliar minha rede de contatos e conhecer
-            pessoas influentes na minha área, e melhorar minhas Soft-Skills e tambem ter o cargo de Gerente de Projetos, o
-            qual me fez desenvolver Hard-Skills e saber como lidar com precificação e documentação de projetos, correção de bugs
-            principalmente em merges de branchs tanto no GitHub qunto no BitBucket e lidar diretamente com clientes e prazos de etrega 
-            e planejamentos em geral.</p>
-          </section>
-
-          <section className={styles.Foto2}>
+          <header>
             <Image alt='Foto da RR' src={foto2}/>
+          </header>
+
+          <section>
+            <h1>Sobre Mim</h1>
+
+            <p>Olá meu nome é Pedro, tenho 19 anos curso Ciência da Computação e trabalho como desenvolvedor e gerente de projetos.</p>
+            <hr/>
+
+            <p>Sou uma pessoa muito comunicativa, com vontade de aprender e dedicação para me manter aprendendo cada dia mais.</p>
+
+            <p>Como desenvolvedor tenho o trabalho de apresentar codigos de facil manutenibilidade, que respeitem determinada
+                arquitetura e atendam todas as necessidades dos clientes de forma intuitiva e com um otimo desenvolvimento de UX e Ui.</p>
+
+            <p>Já como Gerente de Projetos acompanho o projeto desde sua precificação, desenvolvimento de sprints e alocação de
+                desenvolvedores, apos o inicio do projeto sou encarregado de delegar tarefas aos desenvolvedores, auxilia-los e
+                orienta-los em possiveis dificuldades e unir em todas as atuais ramificações do codigo em um arquivo principal,
+                sempre garantindo a qualidade do produto.</p>
+
+            <p>Tive a oportunidade de participar de alguns eventos empresariais que me permitiram ampliar minha rede de contatos e
+                conhecer pessoas influentes na minha área, e melhorar minhas Soft-Skills.</p>
           </section>
-
-          
-
-        </section>
-        <Navbar/>
         
+        </main>
       </>  
     )
   }
